be81e9dbe1f8fb39dc98a9947a1e0e703c96c98f
Nick McComb <nrpickle@gmail.com>
2015-12-22 16:55:39 -0800

    Test Blink

--- PDB/PowerDistribution/PowerDistribution/Debug/main.d
+++ PDB/PowerDistribution/PowerDistribution/Debug/main.d
@@ -10,7 +10,10 @@ main.d main.o: .././main.cpp \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\xmega.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -35,3 +38,9 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- PDB/PowerDistribution/PowerDistribution/main.cpp
+++ PDB/PowerDistribution/PowerDistribution/main.cpp
@@ -5,16 +5,35 @@
  * Author : nrpic_000
  */ 
 
+#define F_CPU 2000000
+
 #include <avr/io.h>
+#include <util/delay.h>
+
+
+#define STATUS_SET(void) (PORTC.OUTSET = PIN0_bm)
+#define STATUS_CLR(void) (PORTC.OUTCLR = PIN0_bm)
+
+#define ERROR_SET(void) (PORTC.OUTSET = PIN1_bm)
+#define ERROR_CLR(void) (PORTC.OUTCLR = PIN1_bm)
 
 
 int main(void)
 {
+	PORTC.DIRSET = PIN1_bm;
+	PORTC.DIRSET = PIN0_bm;
 	
+	STATUS_SET();
 	
     /* Replace with your application code */
     while (1) 
     {
+		_delay_ms(500);
+		ERROR_SET();
+		STATUS_CLR();
+		_delay_ms(500);
+		STATUS_SET();
+		ERROR_CLR();
     }
 }
 
