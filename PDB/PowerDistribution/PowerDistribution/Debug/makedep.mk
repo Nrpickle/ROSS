4f361f22f31c8948400667883514aea944749d23
Nick McComb <nrpickle@gmail.com>
2015-12-30 11:51:14 -0800

    Enabled 32Mhz Internal Oscillator

--- PDB/PowerDistribution/PowerDistribution/Debug/makedep.mk
+++ PDB/PowerDistribution/PowerDistribution/Debug/makedep.mk
@@ -4,3 +4,5 @@
 
 main.cpp
 
+usart_driver.c
+
--- PDB/PowerDistribution/PowerDistribution/main.cpp
+++ PDB/PowerDistribution/PowerDistribution/main.cpp
@@ -8,7 +8,7 @@
  * Author : Nick McComb | nickmccomb.net
  */ 
 
-#define F_CPU 2000000
+#define F_CPU 32000000
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -16,11 +16,15 @@
 
 //Function Prototypes
 void initIO();
+void configureExternalOscillator();
+void configure32MhzInternalOsc();
 
 int main(void)
 {
 	initIO();
-
+	//configureExternalOscillator();
+	configure32MhzInternalOsc();
+	
     while (1) 
     {
 		_delay_ms(500);
@@ -35,19 +39,59 @@ int main(void)
     }
 }
 
+void configureExternalOscillator(){
+	
+	//Configure for 4Mhz external crystal. Take 16K clock cycle because startup time doesn't matter.
+	OSC_XOSCCTRL = OSC_FRQRANGE_2TO9_gc | OSC_XOSCSEL_XTAL_16KCLK_gc;
+	
+	//Enable external oscillator
+	OSC_CTRL |= OSC_XOSCEN_bm;
+	
+	//Wait for clock stabilization
+	while(!(OSC_STATUS & OSC_XOSCRDY_bm));
+	
+	/*
+	
+	//Set PLL multiplication of 8 (4Mhz * 8 = 32Mhz)
+	//PLLFAC is the lowest significant 5 bits
+	OSC.PLLCTRL |= 8;
+	
+	//Set PLL input source (external clock)
+	OSC.PLLCTRL |= OSC_PLLSRC_XOSC_gc;
+	
+	//Enable the PLL
+	OSC.CTRL |= OSC_PLLEN_bm;
+	
+	//Wait for PLL to stabilize
+	while(!(OSC_STATUS & OSC_PLLRDY_bm));
+	*/
+	
+}
  
 void initIO(void){
-		//Set STATUS and ERROR LEDs to be outputs
-		PORTC.DIRSET = PIN1_bm;
-		PORTC.DIRSET = PIN0_bm;
+	//Set STATUS and ERROR LEDs to be outputs
+	PORTC.DIRSET = PIN1_bm;
+	PORTC.DIRSET = PIN0_bm;
+	
+	//Set the Relay Pin to be an output
+	PORTC.DIRSET = PIN5_bm;
+	
+	//Initialize output values
+	STATUS_CLR();
+	ERROR_CLR();
+	
+	REAR_RELAY_CLR();
 		
-		//Set the Relay Pin to be an output
-		PORTC.DIRSET = PIN5_bm;
-		
-		//Initialize output values
-		STATUS_CLR();
-		ERROR_CLR();
-		
-		REAR_RELAY_CLR();
-		
+}
+
+void configure32MhzInternalOsc()
+{
+	OSC_CTRL |= OSC_RC32MEN_bm; //Setup 32Mhz crystal
+	
+	while(!(OSC_STATUS & OSC_RC32MRDY_bm));
+	
+	CCP = CCP_IOREG_gc; //Trigger protection mechanism
+	CLK_CTRL = CLK_SCLKSEL_RC32M_gc; //Enable internal  32Mhz crystal
+	
+	
 }
