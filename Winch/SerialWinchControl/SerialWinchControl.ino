790b80c400bd179c43f8ad4295d789bcfef1b820
RobJShannon <rj_shannon@comcast.net>
2016-02-25 14:49:56 -0800

    remove test code
    This is the first fully functioning version of the Windows serial
    communication code for Arduino. Does not utilize checksum or error
    checking.

--- Winch/SerialWinchControl/SerialWinchControl.ino
+++ Winch/SerialWinchControl/SerialWinchControl.ino
@@ -25,33 +25,48 @@ void loop() {
   while(!Serial.available()); //Wait for serial input
   
   int parameters[8];
+  int incomingByte = 0;
   
   while(Serial.available() < 8) {} //wait for buffer to fill then save values in parameter array
   for (int i = 0; i < 8; i++){
-    parameters[i] = Serial.read();
+    incomingByte = Serial.read();
+    parameters[i] = incomingByte;
     digitalWrite(13, HIGH);
   }
-  delay(1000);
+  delay(500);
   digitalWrite(13, LOW);
   
   int winchSpeed = parameters[1];
+  //char testSend = (char) winchSpeed;
+  //Serial.write(testSend);
   int depthByte1 = parameters[2];
+  //Serial.write(depthByte1);
   int depthByte2 = parameters[3];
+  //Serial.write(depthByte2);
   int duration = parameters[4];
+  Serial.write(duration);
   int units = parameters[5];
+  //Serial.write(units);
   int checksum = parameters[6];
-  
+  //Serial.write(checksum);
+
+  /*
   if(checksum != ((((winchSpeed ^ depthByte1) ^ depthByte2) ^ duration) ^ units)){
     Serial.print("ERROR: Data is corrupted.");
     return;
   }
   else
     Serial.print("Data transmissions successful.");
-    
+  */  
+  
   int depth = depthByte1 + depthByte2;
-  int maxSpeed = winchSpeed*90/255;
+  //int depth = 500000; //Test depth
+  depth = depth * 1000; //pings/meter ratio (current is place holder)
+  int maxSpeed = winchSpeed*90/254;
   int maxWinchSpeedOut = 90 + maxSpeed;
+  //int maxWinchSpeedOut = 180; //Test max speed
   int maxWinchSpeedIn = 90 - maxSpeed;
+  //int maxWinchSpeedIn = 0; //test 
   
   if (units == 0){
     duration = duration * 1000;
@@ -59,12 +74,12 @@ void loop() {
   else if (units == 255){
     duration = duration * 60000;
   }
-  else{
+  /*else{
     Serial.write("ERROR: Invalid duration units provided.");
     duration = 0;
     return;
-  }
-  
+  }*/
+
   while(winchEncoder.read() < (depth - 200000))
     ESC.write(maxWinchSpeedOut);
   while(winchEncoder.read() < (depth - 100000))
@@ -72,7 +87,7 @@ void loop() {
   while(winchEncoder.read() < (depth - 10000))
     ESC.write(maxWinchSpeedOut*.7);
   while(winchEncoder.read() < (depth))
-    ESC.write(maxWinchSpeedOut*.5); 
+    ESC.write(maxWinchSpeedOut*.6); 
   ESC.write(90);
   
   delay(duration);
@@ -86,5 +101,5 @@ void loop() {
   while(winchEncoder.read() > 0)
     ESC.write(maxWinchSpeedIn + .8*maxSpeed);
   ESC.write(90);
-  winchEncoder.write(0);
+  winchEncoder.write(0);  
 }
