c61a9eb8f9551f21e7f95f731d1b7d86ba211bd0
RobJShannon <rj_shannon@comcast.net>
2016-04-01 18:04:44 -0700

    Update Winch Control to utilize reed switches
    Issue with switches activating due to vibration. Exploring debounce
    options.

--- Winch/SerialWinchControl/SerialWinchControl.ino
+++ Winch/SerialWinchControl/SerialWinchControl.ino
@@ -2,9 +2,14 @@
 
 #include <Servo.h>
 #include <Encoder.h>
+#include <Bounce2.h>
 
 Servo ESC;
 Encoder winchEncoder(2,3);
+
+#define reed1 14
+#define reed2 15
+#define debounceTime 10 //set minimum length of time a switch must be held (in ms)
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,6 +22,8 @@ void setup() {
   ESC.write(90);
   delay(6000);
   pinMode(13, OUTPUT);
+  pinMode(reed1, INPUT);
+  pinMode(reed2, INPUT);
 }
 
 void loop() {
@@ -43,7 +50,7 @@ void loop() {
   int checksum = parameters[5];
  
   /*
-  if(checksum != ((((winchSpeed ^ upperByte) ^ lowerByte) ^ duration) ^ units)){
+  if(checksum != (((winchSpeedOut ^ winchSpeedIn) ^ upperByte) ^ lowerByte)){
     Serial.print("ERROR: Data is corrupted.");
     return;
   }
@@ -57,29 +64,51 @@ void loop() {
   
   int maxSpeedOut = winchSpeedOut*90/254;
   int maxSpeedIn = winchSpeedIn*90/254;
-  int maxWinchSpeedOut = 90 + maxSpeed;
-  int maxWinchSpeedIn = 90 - maxSpeed;
-
-  while(winchEncoder.read() < (depth - 200000))
-    ESC.write(maxWinchSpeedOut);
+  int maxWinchSpeedOut = 90 + maxSpeedOut;
+  int maxWinchSpeedIn = 90 - maxSpeedIn;
+  
+  bool reedSwitch1 = debounce(digitalRead(reed1), reed1);
+  while(reedSwitch1){
+    ESC.write(115);
+    reedSwitch1 = debounce(digitalRead(reed1), reed1);
+  }
   while(winchEncoder.read() < (depth - 100000))
+    ESC.write(maxWinchSpeedOut);
+  while(winchEncoder.read() < (depth - 50000))
     ESC.write(maxWinchSpeedOut*.9); 
   while(winchEncoder.read() < (depth - 10000))
-    ESC.write(maxWinchSpeedOut*.7);
+    ESC.write(maxWinchSpeedOut*.8);
   while(winchEncoder.read() < (depth))
-    ESC.write(maxWinchSpeedOut*.6); 
+    ESC.write(maxWinchSpeedOut*.7); 
   ESC.write(90);
   
-  delay(duration);
-  
-  while(winchEncoder.read() > 200000)
-    ESC.write(maxWinchSpeedIn);
+  delay(1000);
+ 
   while(winchEncoder.read() > 100000)
-    ESC.write(maxWinchSpeedIn + .2*maxSpeed);
+   ESC.write(maxWinchSpeedIn);
+  while(winchEncoder.read() > 50000)
+    ESC.write(maxWinchSpeedIn + .2*maxSpeedIn);
   while(winchEncoder.read() > 10000)
-    ESC.write(maxWinchSpeedIn + .5*maxSpeed);
-  while(winchEncoder.read() > 0)
-    ESC.write(maxWinchSpeedIn + .8*maxSpeed);
+    ESC.write(maxWinchSpeedIn + .5*maxSpeedIn);
+//  while (!debounce(digitalRead(reed1),reed1))
+//    ESC.write(maxWinchSpeedIn + .5*maxSpeedIn);
+  while(debounce(digitalRead(reed2),reed2))
+    ESC.write(65);
   ESC.write(90);
-  winchEncoder.write(0);  
+  winchEncoder.write(0); //Reset encoder to reduce error propagation. 
+}
+
+//This debounce function will mitigate erronious switch activations
+//bool debounce(bool readVal, bool pin){
+//  bool currentVal = digitalRead(pin);
+//  if(currentVal != readVal){
+//    delay(debounceTime);
+//    currentVal = digitalRead(pin);
+//  }
+//  return currentVal;
+//}
+
+bool debounce(bool readVal, bool pin){
+  if(readVal != prevVal)
+    lastDebounceTime = millis();
 }
