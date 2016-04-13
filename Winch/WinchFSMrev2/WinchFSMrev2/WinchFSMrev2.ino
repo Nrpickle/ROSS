be66eb52703e743b5b36838c33ffeb185e09d54f
RobJShannon <rj_shannon@comcast.net>
2016-04-13 14:56:33 -0700

    Complete first working FSM release
    Working operation parameters include: Take Profile, Fast In, Slow In,
    Halt, Remote Start, and Remote Stop. Next revision will include serial
    feedback.

--- Winch/WinchFSMrev2/WinchFSMrev2/WinchFSMrev2.ino
+++ Winch/WinchFSMrev2/WinchFSMrev2/WinchFSMrev2.ino
@@ -9,8 +9,8 @@
 #define sendStatus 2
 
 //Define remote start/stop pins
-#define remoteStart 5
-#define remoteStop 6
+#define remoteStartPin 5
+#define remoteStopPin 6
 #define remoteStartLED 7
 #define remoteStopLED 8
 #define startTime 750 //How long to hold remote start HIGH
@@ -28,17 +28,19 @@ int upperByte;
 int lowerByte;
 int checksum;
 int buffSize = 0;
-int speedOut; //
-int speedIn; //
-int depth;
+int speedOut;
+int speedIn;
+long long depth;
 bool motorRunning = false;
 bool depthReached = false;
+bool softStopped = false;
+bool halt = false;
 
 void setup() {
   // put your setup code here, to run once:
-  delay(5000); //Increase for final revision
-  Serial.begin(9600);
-  ESC.attach(9);
+   Serial.begin(9600);
+  delay(5000); //Ensure the winch is powered on before calibration sequence begins //Increase for final revision
+  ESC.attach(9);//Begin calibration sequence
   ESC.write(180);
   delay(4000);
   ESC.write(0);
@@ -46,14 +48,14 @@ void setup() {
   ESC.write(90);
   delay(6000);
   
-  pinMode(13, OUTPUT);
-  pinMode(remoteStart, OUTPUT);
-  pinMode(remoteStop, OUTPUT);
+  pinMode(13, OUTPUT); //Remove for final revision
+  pinMode(remoteStartPin, OUTPUT);
+  pinMode(remoteStopPin, OUTPUT);
   pinMode(remoteStartLED, OUTPUT);
   pinMode(remoteStopLED, OUTPUT);
   
-  digitalWrite(remoteStart, LOW);
-  digitalWrite(remoteStop, HIGH);
+  digitalWrite(remoteStartPin, LOW);
+  digitalWrite(remoteStopPin, HIGH);
   digitalWrite(remoteStartLED, LOW);
   digitalWrite(remoteStopLED, LOW);
 }
@@ -62,46 +64,40 @@ void loop() {
   // put your main code here, to run repeatedly:
   switch(state){
     case checkBuffer:
-      if(buffSize == 7){
-        header = parameters[0];
-        speedOut = parameters[1]; //Save array contents to corisponding variables
-        speedIn = parameters[2];
-        upperByte = parameters[3];
-        lowerByte = parameters[4];
-        checksum = parameters[5];
-        buffSize = 0;
-      
-        upperByte = upperByte << 8;
-        depth = upperByte + lowerByte;
-        depth = depth * 1000; //pings/meter ratio (current is place holder)
-        
-        speedOut = 90 + (speedOut*90/254);
-        speedIn = 90 - (speedIn*90/254);
-        digitalWrite(13,HIGH);
-      }
+      if(buffSize == 7)
+        updateParameters();
       state = controlWinch;
      break;
       
     case controlWinch:
-      if(header == 255){
-        if(depthReached == false){
-          if(winchEncoder.read()<depth)
-            ESC.write(speedOut);
-          else if(winchEncoder.read()>= depth){
-            ESC.write(90);
-            depthReached = true;
-          }
-        }
-        else if(depthReached == true){
-          if(winchEncoder.read()>0)
-            ESC.write(speedIn);
-          else if(winchEncoder.read()<=0){
-            ESC.write(90);
-            depthReached = false;
-          }  
-        }
+      if(header == 255)//Take a profile - normal operation
+        takeProfile();
+      else if(header == 0xAA){//STOP:Return at full speed
+        if(softStopped == false)
+          softStopped = softStop();
+        depthReached = true;
+        speedIn = 0;
+        takeProfile();
       }
-    
+      else if(header == 0xBB){//STOP:Return at half speed
+        if(softStopped == false)
+          softStopped = softStop();
+        depthReached = true;
+        speedIn = 45;
+        takeProfile();
+      }
+      else if(header == 0xCC){
+        if(softStopped == false)
+          softStopped = softStop();
+        halt = true;
+        depthReached = true;
+      }
+      else if(header == 0xDD)
+        remoteStart();
+      else if(header == 0xEE)
+        remoteStop();
+      state = checkBuffer;
+    break;
     
     
   }
@@ -109,6 +105,110 @@ void loop() {
 
 
 void serialEvent(){
-  parameters[buffSize] = Serial.read();
-  buffSize++;
+  if(Serial.available()){
+    parameters[buffSize] = Serial.read();
+    buffSize++;
+  }
+}
+
+void updateParameters(){
+  header = parameters[0];
+  speedOut = parameters[1]; //Save array contents to corisponding variables
+  speedIn = parameters[2];
+  upperByte = parameters[3];
+  lowerByte = parameters[4];
+  checksum = parameters[5];
+  buffSize = 0; //Reset buffer size and control variables
+  depthReached = false;
+  softStopped = false;
+  halt = false;  
+  upperByte = upperByte << 8;
+  depth = upperByte + lowerByte;
+  depth = depth * 1000; //pings/meter ratio (current is place holder)
+  speedOut = 90 + (speedOut*90/254);
+  speedIn = 90 - (speedIn*90/254);
+}
+
+void takeProfile(){
+   if(depthReached == false){
+    if(winchEncoder.read() < (depth - 40000)) //reduce speed as sensor nears destination
+      ESC.write(speedOut);
+    else if(winchEncoder.read() < (depth - 30000))
+      ESC.write(min(160, speedOut));
+    else if(winchEncoder.read() < (depth - 20000))
+      ESC.write(min(145, speedOut));
+    else if(winchEncoder.read() < (depth - 10000))
+      ESC.write(min(130, speedOut));
+    else if((winchEncoder.read() < depth))
+      ESC.write(min(115, speedOut));
+    else if(winchEncoder.read()>= depth){
+      ESC.write(90);
+      depthReached = true;
+      delay(500);//Delay half a second to reduce mechanical stress
+    }
+  }
+  else if(depthReached == true && halt == false){
+    if(winchEncoder.read() > 40000)
+      ESC.write(speedIn);
+    else if(winchEncoder.read() > 30000)
+      ESC.write(max(20, speedIn));
+    else if(winchEncoder.read() > 20000)
+      ESC.write(max(35, speedIn));
+    else if(winchEncoder.read() > 10000)
+      ESC.write(max(50, speedIn));
+    else if(winchEncoder.read() > 0)
+      ESC.write(max(65, speedIn));
+    else if(winchEncoder.read() <= 0)
+      ESC.write(90);  
+  }
+}
+
+bool softStop(){
+  int currentSpeed = ESC.read();
+  if(currentSpeed > 90){ //If letting line out
+    for(int x = currentSpeed; x > 90; x = x - 10){
+      ESC.write(x);
+      delay(100); //Reduce mechanical wear
+    }
+    ESC.write(90); //Bring to a stop
+    delay(500);
+    return true;
+  }
+  else if(currentSpeed < 90){ //If taking line in
+    for(int x = currentSpeed; x < 90; x = x + 10){
+      ESC.write(x);
+      delay(100);
+    }
+    ESC.write(90);
+    delay(500);
+    return true;
+  }
+  else{
+    ESC.write(90);
+    return true;
+  }
+}
+
+  
+void remoteStart(){
+  if (motorRunning == false){ //Prevent remote start from executing if motor already running
+    digitalWrite(remoteStopPin, LOW);
+    digitalWrite(remoteStartPin, HIGH);
+    digitalWrite(remoteStartLED, HIGH);
+    delay(startTime);
+    digitalWrite(remoteStartPin, LOW);
+    delay(2000-startTime);
+    digitalWrite(remoteStartLED, LOW);
+    motorRunning = true;
+  }
+}
+
+void remoteStop(){
+  if(motorRunning == true){
+    digitalWrite(remoteStopPin, HIGH);
+    digitalWrite(remoteStopLED, HIGH);
+    delay(2000);
+    digitalWrite(remoteStopLED, LOW);
+    motorRunning = false;
+  }
 }
