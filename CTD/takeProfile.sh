3c952ad31ff55b72a25eb0ca8f861240950e8fb6
RobJShannon <rj_shannon@comcast.net>
2016-05-23 17:07:41 -0700

    Update profile downloading script
    Make profile names more unique and fix size variable contents and name.

--- CTD/takeProfile.sh
+++ CTD/takeProfile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-size = wc -c /home/pi/CTD/LogFiles/combinedProfile.txt
-./logger2wifidownloader -o $size -l all | /home/pi/CTD/ParseReader/a.out 6 > /home/pi/CTD/LogFiles/placeHolder.txt
- cat /home/pi/CTD/LogFiles/placeHolder.txt > /home/pi/CTD/LogFiles/Profile-$(date +%Y%m%d).txt 
+profileSize=$(wc --bytes < /home/pi/CTD/LogFiles/combinedProfile.txt)
+profileSize=$((profileSize/1))
+echo $profileSize
+./logger2wifidownloader -offset=$profileSize -length=all | /home/pi/CTD/ParseReader/a.out 6 > /home/pi/CTD/LogFiles/placeHolder.txt
+ cat /home/pi/CTD/LogFiles/placeHolder.txt > /home/pi/CTD/LogFiles/Profile-$(date +%Y%m%d%H%M%S).txt 
  cat /home/pi/CTD/LogFiles/placeHolder.txt >> /home/pi/CTD/LogFiles/combinedProfile.txt
