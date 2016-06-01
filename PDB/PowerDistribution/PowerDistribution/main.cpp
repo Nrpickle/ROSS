8287a71e28c71a0d7f2513c1599db08bc20cb1d7
Nick McComb <nick@nickmccomb.net>
2016-06-01 12:13:41 -0700

    Gitignore Update

--- PDB/PowerDistribution/PowerDistribution/main.cpp
+++ PDB/PowerDistribution/PowerDistribution/main.cpp
@@ -73,9 +73,9 @@ int main(void)
 				REAR_RELAY_CLR();
 		}		
 		
-		//if(USART_IsRXComplete(&ONOFF_USART)){
+		if(USART_IsRXComplete(&ONOFF_USART)){
 			
-		//}
+		}
 		
 		//TODO: if(pixhawkControl)
 		TC_PWM_SET(steeringPWMPeriod);	//Process the artificial PWM output
