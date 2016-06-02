3374fbdeca4fa51710cb53ebeb69b928b85717eb
RobJShannon <rj_shannon@comcast.net>
2016-06-01 17:14:56 -0700

    Update bash file
    This file is now outdated. Please use downloadProfile.sh instead.

--- CTD/takeProfile.sh
+++ CTD/takeProfile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 profileSize=$(wc --bytes < /home/pi/CTD/LogFiles/combinedProfile.txt)
-profileSize=$((profileSize/1))
+binaryProfileSize=$((((profileSize/32)+1)*32))
 echo $profileSize
-./logger2wifidownloader -offset=$profileSize -length=all | /home/pi/CTD/ParseReader/a.out 6 > /home/pi/CTD/LogFiles/placeHolder.txt
- cat /home/pi/CTD/LogFiles/placeHolder.txt > /home/pi/CTD/LogFiles/Profile-$(date +%Y%m%d%H%M%S).txt 
- cat /home/pi/CTD/LogFiles/placeHolder.txt >> /home/pi/CTD/LogFiles/combinedProfile.txt
+timeStamp=$(date +%Y%m%d%H%M%S)
+./logger2wifidownloader -offset=$profileSize -length=all > /home/pi/CTD/LogFiles/placeHolder.txt
+cat /home/pi/CTD/LogFiles/placeHolder.txt > /home/pi/CTD/LogFiles/Profile-$timeStamp.txt  
+cat /home/pi/CTD/LogFiles/placeHolder.txt >> /home/pi/CTD/LogFiles/combinedProfile.txt
+cat /home/pi/CTD/LogFiles/combinedProfile.txt | /home/pi/CTD/ParseReader/a.out 6 > /home/pi/CTD/LogFiles/combinedParsedProfiles.txt
+cat /home/pi/CTD/LogFiles/Profile-$timeStamp.txt | /home/pi/CTD/ParseReader/a.out 6 > /home/pi/CTD/LogFiles/parsedProfile-$timeStamp.txt
