487f6b47c05437e53ecc4027a288786156fb0053
Nick McComb <nick@nickmccomb.net>
2016-06-28 15:18:27 -0700

    Made winch code less broken (accelerate)

--- Winch/accelerate/accelerate.ino
+++ Winch/accelerate/accelerate.ino
@@ -1,11 +1,11 @@
-typedef struct {
+struct Winch_TYPE {
   uint8_t currentSpeed;
   uint8_t prevSpeed;
-  uint8_t currentDir; // up, down, stationary
+  uint8_t currentDir; // up, down, stop
   uint8_t prevDir;
   bool newChange;
   //uint8_t updated;
-} Winch;
+} winch;
 
 //#include <Stdint.h>
 #include <Servo.h>
@@ -24,14 +24,17 @@ enum{
 #define MAX_FORWARD 1910 //Maximum, minimum, and neutral pulse widths in microseconds
 #define NEUTRAL 1479
 #define MAX_REVERSE 1048
-#define RAMP_TIME 500 //Time it takes to change speed in milliseconds
+//#define RAMP_TIME 500 //Time it takes to change speed in milliseconds
 #define REV(x) 3936*x //Converts revolutions into encoder pings
 
+const double RAMP_TIME = 500;
 const float pi = 3.14159;
 uint64_t t0 = 0; //Beginning time for speed change
 uint8_t speedDifference = 0; //Difference between desired and current speed
 bool destReached = false;
-Winch winch; //= {
+
+
+//= {
 //    .currentSpeed = 100
 //    .prevSpeed = 100
 //    .currentDir = STOP // up, down, stop
@@ -55,57 +58,88 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //if(millis() < 15000)
-  //if(winchEncoder.read() <= REV(15) //&& destReached == false){
+  //Testing code
+  
+  if(millis()<15000)
     changeSpeed(50, DOWN);
-//  if(winchEncoder.read() > REV(15)){
-//    changeSpeed(100, UP);
-//    destReached = true;
-//  }
-//  if(winchEncoder.read() <= REV(0)){
-//    changeSpeed(0, STOP);
-//    winchEncoder.write(0);
-//    destReached = false;
-//  }
-//  else{
-//    changeSpeed(0, STOP);
+  else if(millis()<20000)
+    changeSpeed(0, STOP);
+  else if(millis()<25000)    
+    changeSpeed(50, UP);
+  else
+    changeSpeed(0, STOP);
   
-  //}
+  if(!winch.newChange){
+    Serial.print("[main loop]");
+    Serial.println(winch.currentSpeed);
+  }
   
+  delay(5);
+  //Serial.println("hi!");
 }
 
 void changeSpeed(uint8_t newSpeed, uint8_t newDir){
-  if(newDir == winch.currentDir && newSpeed == winch.currentSpeed){
-    winch.prevSpeed = winch.currentSpeed;
-    winch.newChange = true;
-    return;
-  }
+
+  //If we want to go UP
   if(newDir == UP){
     newSpeed = 100 - newSpeed;
     winch.currentDir = UP;
   }
+  //If we want to go down
   else if(newDir == DOWN){
     newSpeed = 100 + newSpeed;
     winch.currentDir = DOWN;
   }
+  //Else we want to STOP
   else{
     newSpeed = 100;
     winch.currentDir = STOP;
   }
-    
+  
+  Serial.print("[comp ");
+  Serial.print(newSpeed);
+  Serial.print(" | ");
+  Serial.print(winch.currentSpeed);
+  Serial.println("]");
+  
+
+  //Check if no change is needed (if we are going the speed we want)
+  if(newDir == winch.currentDir && newSpeed == winch.currentSpeed){  //If the command is to continue moving the same speed...
+    winch.prevSpeed = winch.currentSpeed;
+    winch.newChange = true;
+    Serial.println("[REACHED END CASE]");
+    return;   //We want to return
+  }
+
+
+  //This will catch if we have gotten to this spot while the previous call to the function was "no change requested"
   if(winch.newChange == true){
+    //If that's the case, then we want to initialize the acceleration
     t0 = millis();
     speedDifference = newSpeed - winch.prevSpeed;
+    
+    if(speedDifference < 0)
+      speedDifference -= 1;
+    else if (speedDifference > 0)
+      speedDifference += 1;
+    
     winch.newChange = false;
   }
    
   uint64_t deltaT = millis() - t0;
-  winch.currentSpeed = winch.prevSpeed + speedDifference*.5*(1-cos((pi*deltaT)/RAMP_TIME));
+  winch.currentSpeed = (double)winch.prevSpeed + (double)speedDifference*.5*(1-cos((pi*(double)deltaT)/RAMP_TIME));
   
-  uint16_t speedToWrite = map(winch.currentSpeed, 0, 200, MAX_FORWARD, MAX_REVERSE);
+  /*
+  if(!winch.newChange){
+    Serial.print("[changeSpeed]");
+    Serial.println(speedDifference);
+  }
+  */
+  
+  uint16_t speedToWrite = map(winch.currentSpeed, 0, 200, MAX_REVERSE, MAX_FORWARD);
   ESC.writeMicroseconds(speedToWrite); 
-  //Serial.println(speedToWrite);
+  
+  Serial.println(speedToWrite);
   
   if(winch.currentSpeed == newSpeed)
     winch.prevSpeed = newSpeed;
